c80fe9855e21a66f5d45d3699b5141ea17910177
Michael <1754003+hipstercoder@users.noreply.github.com>
2018-05-28 08:50:17 +0200

    report power usage of each gpu

--- ethminer-monitor.py
+++ ethminer-monitor.py
@@ -14,23 +14,21 @@ def get_miner_info():
 	global hashrate
 	global hardware
 	global temperature_gpu
+	global power_usage
 	global fanspeed_gpu
 	global pool
 
-	cmd = '''echo '{"method": "miner_getstat1", "jsonrpc": "2.0", "id": 5 }' | timeout 2 nc 127.0.0.1 8085'''
+	cmd = '''echo '{"method": "miner_getstathr", "jsonrpc": "2.0", "id": 5 }' | timeout 2 nc 127.0.0.1 8085'''
 	s = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 	rsp = s.communicate()[0]
 	data = json.loads(rsp)
-	version = data['result'][0]
-	uptime = data['result'][1]
-	hashrate = data['result'][3]
-	hashrate = hashrate.split(";")
-	hardware = data['result'][6]
-	hardware = hardware.split(";")
-	temperature_gpu = hardware[::2]
-	fanspeed_gpu = hardware[1::2]
-	pool = data['result'][7]
-	pool = pool.split(";")
+	version = data['result']['version']
+	uptime = data['result']['runtime']
+	hashrate = data['result']['ethhashrates']
+	power_usage = data['result']['powerusages']
+	temperature_gpu = data['result']['temperatures']
+	fanspeed_gpu = data['result']['fanpercentages']
+	pool = data['result']['pooladdrs']
 
 def print_result():
 
@@ -39,7 +37,8 @@ def print_result():
 	gpu = 0
 	for item in hashrate:
 		if int(item) < 20000:
-		problem = True
+			problem = True
+		item = int(item) / 1000
 		tmp = "hashrate_gpu" + str(gpu) + "=" + str(item)
 		result.append(tmp)
 		gpu = gpu + 1
@@ -47,8 +46,16 @@ def print_result():
 	gpu = 0
 	for item in temperature_gpu:
 		if int(item) > 70:
-		problem = True
-		tmp = "temperature_gpu" + str(gpu) + "=" + str(item.lstrip())
+			problem = True
+		tmp = "temperature_gpu" + str(gpu) + "=" + str(item)
+		result.append(tmp)
+		gpu = gpu + 1
+
+	gpu = 0
+	for item in power_usage:
+		if int(item) > 120:
+			problem = True
+		tmp = "powerusage_gpu" + str(gpu) + "=" + str(item)
 		result.append(tmp)
 		gpu = gpu + 1
 
@@ -58,22 +65,22 @@ def print_result():
 		result.append(tmp)
 		gpu = gpu + 1
 
-	for item in pool:
-		tmp = "pool=" + str(item)
-		result.append(tmp)
+	item = pool
+	tmp = "pool=" + str(item)
+	result.append(tmp)
 
 	if problem == True:
 		print("Something is wrong with this rig |"),
 		print("version=" + str(version) + " uptime=" + str(uptime)),
 		for item in result:
-	    	print item,
+	    		print item,
 		sys.exit(1)
 	else:
-		print("This mining rig is operating in its specified parameters |"),
+		print("This mining rig is operating in its specified parameters on " + str(pool) + " |"),
 		print("version=" + str(version) + " uptime=" + str(uptime)),
 		for item in result:
-	    	print item,
-    		sys.exit(0)
+	    		print item,
+    	sys.exit(0)
 
 get_miner_info()
 print_result()
